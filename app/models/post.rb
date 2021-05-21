1512d7f9e3a3640df5921582cc03e107fcd7a60d
iaconom821 <iaconom821@gmail.com>
2021-05-20 20:09:46 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait
+
+    def is_clickbait 
+        words = ["Won't Believe", "Secret", "Top ", "Guess"]
+
+        if !title
+            return
+        end
+
+        present = words.any?{|word| title.match(word)}
+
+        if !present
+            errors.add(:title, 'not "clickbaity" enough')
+        end
+    end
+
 end
